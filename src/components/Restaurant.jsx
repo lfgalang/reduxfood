8c69bf08af881cdffcc071c5685d7cf0c8e53fbb
lfgalang <galanpipe3@gmail.com>
2021-10-03 18:35:10 -0500

    has been added the state functions for the form inputs

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Restaurant() {
+
+    const [restaurantState, setRestauranteState] = useState({
+
+    })
+
     return (
         <div>
-            Prueba
+            <form>
+                <div className="form-group">
+                    <label htmlFor="">Name</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        onChange = {(event) => {
+                            setRestauranteState({
+                                ...restaurantState,
+                                ...{name: event.target.value}
+                            })
+                        }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Location</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        onChange = {(event) => {
+                            setRestauranteState({
+                                ...restaurantState,
+                                ...{location: event.target.value}
+                            })
+                        }}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+                {JSON.stringify(restaurantState)}
+            </form>
         </div>
     )
 }
